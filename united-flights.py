b6b2263eada2bf988d76983a3d16f6acd34fce1d
Curtis Brown <downtownslm@gmail.com>
2022-10-05 13:27:40 -0400

    add METAR data for other timestamps

--- united-flights.py
+++ united-flights.py
@@ -3,8 +3,14 @@ import pandas as pd
 
 cols = ['FLIGHTDATE',
         'FLIGHTNO',
+        'ORIGIN',
         'DEST',
-        'DESTOBSERVATIONTIME']
+        'DEPARTUREDATE_ZULU', # SCH DEPARTURE TIME
+        'ARRIVALDATE_ZULU', # SCH ARRIVAL TIME
+        'ORIGINOBSERVATIONTIME', # UA WX ORIGIN OBSERVED TIME
+        'DESTOBSERVATIONTIME', # UA WX DEST OBSERVED TIME
+        'OUT_ZULU', # ACTUAL DEPARTURE TIME
+        'IN_ZULU'] # ACTUAL ARRIVAL TIME
 
 flights = pd.read_excel('data/united/united-flights-2018-2019.xlsx')
 
